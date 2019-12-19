4cd0dbb1ef55d4773f0fa3baa6395fd52775a37c
Hatemii <hatem.xhezairi96@gmail.com>
2019-12-19 12:46:10 +0100

    Last files before MASTER branch!

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,7 +1,7 @@
 class BlogsController < ApplicationController
-  before_action :set_blog, only: [:show, :edit, :update, :destroy]
+  before_action :set_blog, only: [:show, :edit, :update, :destroy, :toggle_status]
 
-  # GET /blogs
+   # GET /blogs
    # GET /blogs.json
    def index
      @blogs = Blog.all
@@ -55,6 +55,16 @@ class BlogsController < ApplicationController
        format.html { redirect_to blogs_url, notice: 'Post was removed.' }
        format.json { head :no_content }
      end
+   end
+
+   def toggle_status
+     if @blog.draft?
+       @blog.published!
+     elsif @blog.published?
+       @blog.draft!
+     end
+
+     redirect_to blogs_url, notice: 'Post status has been updated.'
    end
 
    private
